2e8b4da950bc9da2d4b3c99b1b0d2814bdadb103
Apeksha Bansod <apeksha.bansod@virtuososofttech.com>
2023-03-21 12:15:13 +0530

    first commit

---
+++ WebCheck/src/main/java/com/vst/WebCheck/App.java
@@ -0,0 +1,13 @@
+package com.vst.WebCheck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
